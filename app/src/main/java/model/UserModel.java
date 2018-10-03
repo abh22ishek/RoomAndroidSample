1bbf9616de7411d0c096082eee30ccf312c21bc4
abh22ishek <abh22ishek@gmail.com>
2018-10-03 17:41:20 +0530

    Initial commit

---
+++ app/src/main/java/model/UserModel.java
@@ -0,0 +1,55 @@
+package model;
+
+public class UserModel {
+
+    String userName;
+    String userPassword;
+    String securityQ1;
+
+    String securityQ2;
+    String securityQ3;
+
+
+
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getSecurityQ1() {
+        return securityQ1;
+    }
+
+    public void setSecurityQ1(String securityQ1) {
+        this.securityQ1 = securityQ1;
+    }
+
+    public String getSecurityQ2() {
+        return securityQ2;
+    }
+
+    public void setSecurityQ2(String securityQ2) {
+        this.securityQ2 = securityQ2;
+    }
+
+    public String getSecurityQ3() {
+        return securityQ3;
+    }
+
+    public void setSecurityQ3(String securityQ3) {
+        this.securityQ3 = securityQ3;
+    }
+}
